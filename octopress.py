3b09f7868669677e1b72b99c100aaa947275dbd9
blueplanet <erguolinge@gmail.com>
2012-10-13 08:22:00 +0900

    change to desync
    - generate command added
    - cursor changed to wait.I's bad.

--- octopress.py
+++ octopress.py
@@ -1,5 +1,6 @@
 import functools
 import os
+import re
 import sublime
 import sublime_plugin
 import subprocess
@@ -9,11 +10,16 @@ import thread
 class OctopressCommand():
 
     def exec_command(self, command):
+        print "octopress exec start."
+
+        self.output = ""
         self.load_config()
 
         os.chdir(self.octopress_path)
 
-        self.proc = subprocess.Popen(self.rake_command + command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        exec_command = "%s %s" % (self.rake_command, command)
+
+        self.proc = subprocess.Popen(exec_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
         thread.start_new_thread(self.read_stdout, ())
 
@@ -26,25 +32,40 @@ class OctopressCommand():
 
         self.rake_command = octo_set.get("rake_command")
 
+    def show_status_message(self, msg):
+        sublime.status_message(msg)
+
     def finish(self):
-        if self.file:
-            print "Open File : " + self.file
-            self.window.open_file(self.octopress_path + self.file)
+        finded = re.search(self.check_str, self.output)
+
+        if finded:
+            sublime.status_message("octopress exec successfully finished.")
+
+            if self.file:
+                print "Open File : " + self.file
+                self.window.open_file(self.octopress_path + self.file)
+        else:
+            sublime.error_message("Octopress exec failed. Please check octopress env, and try argin.\n\nYou can check exec log in sublime console.")
 
     def read_stdout(self):
         while True:
+            sublime.set_timeout(functools.partial(self.show_status_message, "octopress runing..."), 0)
+
             data = os.read(self.proc.stdout.fileno(), 2 ** 15)
 
             if data != "":
-                if data.startswith(self.exec_result):
+                print data
+
+                if self.exec_result and data.startswith(self.exec_result):
                     self.file = data.split(os.linesep)[0].replace(self.exec_result, "")
 
-                print "octopress exec output : " + data
+                self.output += data
             else:
                 self.proc.stdout.close()
 
-                sublime.set_timeout(functools.partial(self.finish), 0)
                 print "octopress exec end."
+
+                sublime.set_timeout(functools.partial(self.finish), 0)
                 break
 
     def read_stderr(self):
@@ -62,8 +83,6 @@ class NewPostCommand(sublime_plugin.WindowCommand, OctopressCommand):
     def run(self):
         self.window.show_input_panel("Enter Name Of New Post", "", self.on_done, None, None)
 
-        print "octopress exec start."
-
     def on_done(self, text):
         command = " \"new_post[%s]\"" % text
 
@@ -75,10 +94,16 @@ class NewPageCommand(sublime_plugin.WindowCommand, OctopressCommand):
     def run(self):
         self.window.show_input_panel("Enter Name Of New Page", "", self.on_done, None, None)
 
-        print "octopress exec start."
-
     def on_done(self, text):
         command = " \"new_page[%s]\"" % text
 
         self.exec_result = "Creating new page: "
         self.exec_command(command)
+
+
+class GenerateCommand(sublime_plugin.WindowCommand, OctopressCommand):
+    def run(self):
+        self.exec_result = ""
+        self.file = ""
+        self.check_str = "Successfully generated site:"
+        self.exec_command("generate")
